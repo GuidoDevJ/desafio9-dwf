311c7ffc64348c7dbd20812ab9c767381f9b74a8
Guido <[guidogauna9@gmail.com]>
2023-02-27 11:26:28 -0300

    fourth commit

--- helper/sendEmailPay.ts
+++ helper/sendEmailPay.ts
@@ -1,15 +1,33 @@
+import { baseVentas } from "lib/airtable";
 import { User } from "models/user";
 import { sendEmailStatusPay } from "./sendrid";
 
-export async function sendStatusPay(userId:string,status:string){
+export async function sendStatusPay(userId: string, status: string,orderId:string) {
     let user = new User(userId)
+    let finalStatus:string;
+    if(status === "closed") finalStatus === "Done"
+
     try {
         await user.pull()
         await sendEmailStatusPay(user.data.email, status)
+        baseVentas('Ventas').create([
+            {
+                "fields": {
+                    "userId": userId,
+                    "orderId": orderId,
+                    "Status": finalStatus
+                }
+            }
+        ], function (err, records) {
+            if (err) {
+                console.error(err);
+                return;
+            }
+        });
         return true
     } catch (error) {
         return error
     }
-   
-    
+
+
 }
--- lib/airtable.tsx
+++ lib/airtable.tsx
@@ -1,2 +1,3 @@
 import Airtable from "airtable"
 export const base = new Airtable({apiKey: 'keyYr1scN1pPdO11e'}).base('appsFZ7F54WL9UHw3');
+export const baseVentas = new Airtable({apiKey: 'keyYr1scN1pPdO11e'}).base('appvMC5w4PK0DJtB8')
